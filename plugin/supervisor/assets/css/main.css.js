c199d81c7221b96d3876eaff3a731fb2d22d0679
epicoon <epicoon@main.ru>
2022-01-13 01:24:25 +0700

    refs #0035 - Refactoring

--- plugin/supervisor/assets/css/main.css.js
+++ plugin/supervisor/assets/css/main.css.js
@@ -1,6 +1,8 @@
 #lx:use lx.CssColorSchema;
 #lx:use lx.MainCssContext;
 
+const cssContext = lx.MainCssContext.cssContext;
+
 cssContext.addClass('lx-process-slot', {
 	overflow: 'hidden',
 	whiteSpace: 'nowrap',
@@ -9,16 +11,16 @@ cssContext.addClass('lx-process-slot', {
 });
 
 cssContext.inheritClasses({
-	'lx-process-send' : { backgroundColor: coldMainColor,    '@icon': ['\\2709', 16] },
-	'lx-process-run'  : { backgroundColor: checkedMainColor, '@icon': ['\\21BB', 16] },
-	'lx-process-stop' : { backgroundColor: neutralMainColor, '@icon': ['\\2296', 16] },
-	'lx-process-close': { backgroundColor: hotMainColor,     '@icon': ['\\2297', 16] }
+	'lx-process-send' : { backgroundColor: lx.CssColorSchema.coldMainColor,    '@icon': ['\\2709', 16] },
+	'lx-process-run'  : { backgroundColor: lx.CssColorSchema.checkedMainColor, '@icon': ['\\21BB', 16] },
+	'lx-process-stop' : { backgroundColor: lx.CssColorSchema.neutralMainColor, '@icon': ['\\2296', 16] },
+	'lx-process-close': { backgroundColor: lx.CssColorSchema.hotMainColor,     '@icon': ['\\2297', 16] }
 }, 'ActiveButton');
 
 cssContext.inheritClasses({
-	'lx-process-status-active':  {backgroundColor: checkedSoftColor},
-	'lx-process-status-closed':  {backgroundColor: neutralSoftColor},
-	'lx-process-status-crashed': {backgroundColor: hotSoftColor}
+	'lx-process-status-active':  {backgroundColor: lx.CssColorSchema.checkedSoftColor},
+	'lx-process-status-closed':  {backgroundColor: lx.CssColorSchema.neutralSoftColor},
+	'lx-process-status-crashed': {backgroundColor: lx.CssColorSchema.hotSoftColor}
 }, 'Input');
 
 return cssContext.toString();
